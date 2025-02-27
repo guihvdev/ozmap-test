16ad6a1a68d0b012d9f3168ed57f69192dee2e25
guilherme <guilherme.viana@nexatlas.com>
2025-02-27 17:33:16 -0300

    test: up close-polygon.validator tests*

--- src/modules/region/validators/business/close-polygon.validator.spec.ts
+++ src/modules/region/validators/business/close-polygon.validator.spec.ts
@@ -1,23 +1,14 @@
 import { expect } from 'chai'
-import sinon from 'sinon'
 import { ClosePolygonValidator } from './close-polygon.validator'
 import { ICreateRegionDto } from '../../../../@protocols/region.protocol'
 import { faker } from '@faker-js/faker'
 import { BadRequestException } from '../../../exceptions/bad-request.exception'
 
 describe('ClosePolygonValidator', () => {
-  let closePolygonValidator: ClosePolygonValidator
+  const instance = new ClosePolygonValidator()
 
-  beforeEach(() => {
-    closePolygonValidator = new ClosePolygonValidator()
-  })
-
-  afterEach(() => {
-    sinon.restore()
-  })
-
-  it('should not thrown if Polygon close correctly', () => {
-    const params: ICreateRegionDto = {
+  it('should thrown BadRequestException if Polygon do not close correctly', () => {
+    const invalidParams: ICreateRegionDto = {
       userId: faker.string.alphanumeric(),
       name: faker.string.alphanumeric(),
       polygon: {
@@ -28,19 +19,38 @@ describe('ClosePolygonValidator', () => {
             [25, 20],
             [25, 25],
             [23, 25],
-            [15, 15]
+            [15, 25]
           ]
         ]
       }
     }
+
     try {
-      closePolygonValidator.validate(params)
-      throw new Error('Expected BadRequestException to be thrown')
+      instance.validate(invalidParams)
     } catch (error) {
       expect(error).to.be.instanceOf(BadRequestException)
-      expect(error.output).to.equal('POLYGON_SHOULD_BE_CLOSED')
+      expect(error.output).to.be.equal('POLYGON_SHOULD_BE_CLOSED')
     }
   })
-  it('should thrown BadRequestException if Polygon close correctly', () => {
+
+  it('should not thrown if Polygon close correctly', () => {
+    const validParams: ICreateRegionDto = {
+      userId: faker.string.alphanumeric(),
+      name: faker.string.alphanumeric(),
+      polygon: {
+        type: 'Polygon',
+        coordinates: [
+          [
+            [20, 20],
+            [25, 20],
+            [25, 25],
+            [23, 25],
+            [20, 20]
+          ]
+        ]
+      }
+    }
+    expect(() => instance.validate(validParams)).to.not.throw()
   })
+
 })
